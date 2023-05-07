dfaf3e3e1bc0d6e5ed37489d8d93c38f55cbd1b6
etandmouse <etandmouse@gmail.copm>
2023-05-07 16:25:51 +0800

    add relations between User and Todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,13 +3,20 @@ const router = express()
 
 const db = require('../../models')
 const Todo = db.Todo
+const User = db.User
 
 router.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos: todos }) })
+  User.findByPk(req.user.id)
+    .then(user => {
+      if (!user) throw new Error('user not found')
+
+      return Todo.findAll({
+        raw: true,
+        nest: true,
+        where: {UserId: req.user.id}
+      })
+    })
+    .then((todos) => { return res.render('index', { todos }) })
     .catch((error) => { return res.status(422).json(error) })
 })
 
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -6,8 +6,11 @@ const Todo = db.Todo
 
 
 router.get('/:id', (req, res) => {
+  const UserId = req.user.id
   const id = req.params.id
-  return Todo.findByPk(id)
+  return Todo.findOne({
+    where: { id, UserId }
+  })
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
