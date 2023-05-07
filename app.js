9d2ee1d8bdc3d0baa51411f5266ee14cdcf49f30
etandmouse <etandmouse@gmail.copm>
2023-05-07 16:07:06 +0800

    add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const flash = require('connect-flash')
 const usePassport = require('./config/passport')
 const routes =require('./routes')
 
@@ -22,9 +23,12 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
